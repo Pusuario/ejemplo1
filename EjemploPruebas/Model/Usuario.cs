4aee8b1c429a48ebb0e1e3c03f348789ab07772d
Pusuario <pusuario@hotmail.com>
2014-06-04 13:22:16 +0200

    Version inicial

---
+++ EjemploPruebas/Model/Usuario.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Web;
+
+namespace EjemploPruebas.Model
+{
+    public class Usuario
+    {
+        public string username { get; set; }
+        public string password { get; set; }
+        public bool activo { get; set; }
+
+        public Usuario()
+        {
+            username = "admin";
+            password = "aaa111...";
+            activo = true;
+        }
+        /// <summary>
+        /// Constructor de usuario. Si el nombre del usuario o la contraseña estan vacios se asignara la contraseña por defecto.
+        /// </summary>
+        /// <param name="user">Nombre de usuario.</param>
+        /// <param name="pass">Contraseña sin cifrar.</param>
+        public Usuario(string user, string pass)
+        {
+            if (user == null || user.Length == 0)
+                user = "admin";
+            username = user;
+            if (pass == null || pass.Length == 0)
+                pass = "aaa111...";
+            password = pass;
+            activo = true;
+        }
+
+        public override string ToString()
+        {
+            return this.username + ";" + this.password;
+        }
+
+        public string getMD5(string pass)
+        {
+            if (pass == null || pass.Length == 0) return null;
+            MD5 md5 = MD5CryptoServiceProvider.Create();
+            ASCIIEncoding en = new ASCIIEncoding();
+            byte[] stream = null;
+            StringBuilder sb = new StringBuilder();
+            stream = md5.ComputeHash(en.GetBytes(pass));
+            for (int i = 0; i < stream.Length; i++)
+                sb.AppendFormat("{0:x2}", stream[i]);
+            return sb.ToString();
+        }
+    }
+}
---
+++ PruebasUnitarias/UsuarioTest.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using EjemploPruebas.Model;
+
+namespace PruebasUnitarias
+{
+    /// <summary>
+    /// Summary description for UsuarioTest
+    /// </summary>
+    [TestClass]
+    public class UsuarioTest
+    {
+        [TestMethod]
+        public void ToStringTest()
+        {
+            Usuario u = new Usuario();
+
+            Assert.AreEqual(u.ToString(), "admin;aaa111...");
+
+        }
+        [TestMethod]
+        public void UsuarioConstructorTest()
+        {
+            Usuario u = new Usuario();
+            Assert.IsNotNull(u);
+            Assert.AreEqual(u.username, "admin");
+            Assert.AreEqual(u.password, "aaa111...");
+        }
+        [TestMethod]
+        [ExpectedException(typeof(Exception))]
+    
+        public void getMD5Test()
+        {
+            Usuario u = new Usuario();
+            string md = u.getMD5("aaa111...");
+            
+            Assert.AreEqual(md, "15827d1b83c6be8de449fa4535737d48");
+
+            md = u.getMD5("");
+            Assert.IsNull(md);
+
+            md = u.getMD5(null);
+            Assert.IsNull(md);
+        }
+    }
+}
